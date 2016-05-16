d833ffd6f235623a2ddcf7f33ae8d7a10f281a7f
iwnow <vostroknutov@hotmail.com>
2016-05-16 18:18:54 -0100

    1

--- src/draw.ts
+++ src/draw.ts
@@ -12,7 +12,7 @@ export class Game {
 			this.scene = new THREE.Scene();
 			this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 			this.renderer = new THREE.WebGLRenderer();
-			this.renderer.setSize( window.innerWidth, window.innerHeight);		
+			this.renderer.setSize( document.body.clientWidth, document.body.clientHeight);		
 			this.renderer.setClearColor(0xEEEEEE);
 			document.getElementById('host').appendChild( this.renderer.domElement );	
 		} catch (e) {
@@ -21,12 +21,17 @@ export class Game {
 	}
 	
 	addObject() {
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0x06ffa0 } );
+		var geometry = new THREE.BoxGeometry( 10, 10, 10 );
+		var material = new THREE.MeshBasicMaterial( { color: 0x06a4a0 } );
 		this.cube = new THREE.Mesh( geometry, material );
 		this.scene.add( this.cube );
-
-		this.camera.position.z = 3;
+		let axes = new THREE.AxisHelper( 20 );
+		this.scene.add(axes);
+		
+		this.camera.position.x = -30;
+		this.camera.position.y = 40;
+		this.camera.position.z = 30;
+		this.camera.lookAt(this.scene.position);
 	}
 	
 	
@@ -34,6 +39,9 @@ export class Game {
 	render() {
 		this.cube.rotation.x += 0.03;
 		this.cube.rotation.y += 0.03;
+		this.camera.position.y -= 0.05;
+		this.camera.position.z -= 0.05;
+		this.camera.position.x += 0.05;
 		requestAnimationFrame( () => {
 			this.render();
 		} );
